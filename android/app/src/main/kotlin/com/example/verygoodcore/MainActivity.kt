8151fde1890cd5b6cbcd61bb5aad6d161a26d2fa
Paulo Victor <paulovieirasilva.1998@gmail.com>
2023-03-25 15:10:19 -0300

    chore: initial commit

---
+++ android/app/src/main/kotlin/com/example/verygoodcore/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.verygoodcore.coffeer_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main_production.dart
@@ -0,0 +1,6 @@
+import 'package:coffeer_app/app/app.dart';
+import 'package:coffeer_app/bootstrap.dart';
+
+void main() {
+  bootstrap(() => const App());
+}
