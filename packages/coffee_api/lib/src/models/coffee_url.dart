f80c7898e18438f0c9c107f56641a44af30f19fb
Paulo Victor <paulovieirasilva.1998@gmail.com>
2023-03-25 16:10:28 -0300

    feat(coffee_api): create CoffeeUrl model and add dependencies to make serializable

---
+++ packages/coffee_api/lib/src/models/coffee_url.dart
@@ -0,0 +1,32 @@
+import 'package:coffee_api/coffee_api.dart';
+import 'package:json_annotation/json_annotation.dart';
+
+part 'coffee_url.g.dart';
+
+@JsonSerializable()
+
+/// {@template coffee_url}
+/// A URL holder for [CoffeeApi]
+///
+/// Example:
+/// ```json
+/// {
+///   "file": "https://coffee.alexflipnote.dev/9hz2zWab3xw_coffee.jpg"
+/// }
+/// ```
+/// {@endtemplate}
+class CoffeeUrl {
+  /// {@macro coffee_url}
+  CoffeeUrl({required this.url});
+
+  /// Factory which converts a [Map<String, Object?>] into a [CoffeeUrl].
+  factory CoffeeUrl.fromJson(Map<String, Object?> json) =>
+      _$CoffeeUrlFromJson(json);
+
+  /// Converts the [CoffeeUrl] to [Map<String, Object?>].
+  Map<String, Object?> toJson() => _$CoffeeUrlToJson(this);
+
+  /// Image URL for [CoffeeUrl]
+  @JsonKey(name: 'file')
+  final String url;
+}
---
+++ packages/coffee_api/lib/src/models/coffee_url.g.dart
@@ -0,0 +1,15 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'coffee_url.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+CoffeeUrl _$CoffeeUrlFromJson(Map<String, dynamic> json) => CoffeeUrl(
+      url: json['file'] as String,
+    );
+
+Map<String, dynamic> _$CoffeeUrlToJson(CoffeeUrl instance) => <String, dynamic>{
+      'file': instance.url,
+    };
