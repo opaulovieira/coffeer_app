fa7a2e33fbe6ca277468d76de8d7a3d7a9fbc42a
Paulo Victor <paulovieirasilva.1998@gmail.com>
2023-03-26 15:59:02 -0300

    chore(key_value_storage): turn key_value_storage package into a flutter one, as it depends on flutter (maybe due to Hive, don't really know)

--- packages/key_value_storage/test/src/key_value_storage_test.dart
+++ packages/key_value_storage/test/src/key_value_storage_test.dart
@@ -1,6 +1,7 @@
 // ignore_for_file: prefer_const_constructors
+
+import 'package:flutter_test/flutter_test.dart';
 import 'package:key_value_storage/key_value_storage.dart';
-import 'package:test/test.dart';
 
 void main() {
   group('KeyValueStorage', () {
