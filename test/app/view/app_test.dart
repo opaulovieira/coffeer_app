6deb50bee5f4cfe171bdba08a7fff8da19d59e48
Paulo Victor <paulovieirasilva.1998@gmail.com>
2023-03-30 20:33:33 -0300

    test(home): remove unused counter page

--- test/app/view/app_test.dart
+++ test/app/view/app_test.dart
@@ -1,12 +1,12 @@
 import 'package:coffeer_app/app/app.dart';
-import 'package:coffeer_app/counter/counter.dart';
+import 'package:coffeer_app/home/home.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 void main() {
   group('App', () {
     testWidgets('renders CounterPage', (tester) async {
       await tester.pumpWidget(const App());
-      expect(find.byType(CounterPage), findsOneWidget);
+      expect(find.byType(HomePage), findsOneWidget);
     });
   });
 }
